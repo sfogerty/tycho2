5085b433e5c0c120a2da96a3d5796af9008a5863
Abida Haque <ahaque34@cori10.nersc.gov>
2018-08-16 15:29:41 -0700

    added experiment single sh file

---
+++ experimentsmemsng.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+for VAR in 12 208 1374 4128 
+##for VAR in 10717 10992 67249 85736 571760 
+do
+cd mesh$VAR
+sbatch -o single.log looptycho2single.sh
+cd ..
+done
+
+sleep 3600
+for VAR in 10717 10992 67249 85736 571760 
+do
+cd mesh$VAR
+sbatch -o single.log looptycho2single.sh
+cd ..
+done
